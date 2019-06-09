e59dd37af31b4eb17ba0910c28fd7748fb25936d
zhangfei <zhangfei@itcast.com>
2019-06-09 20:07:43 +0800

    副程序也修改了代码

--- src/com/Resp.java
+++ src/com/Resp.java
@@ -6,5 +6,6 @@ public class Resp {
         System.out.println("分支1");
         System.out.println("分支2");
         System.out.println("这是个新的resp类");
+        System.out.println("副程序也更新了");
     }
 }
